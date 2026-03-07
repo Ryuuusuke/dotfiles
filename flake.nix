dc1167f99bf1c5a727026ded5c7b3054f0e29280
Ryuuusuke <rryu@national.shitposting.agency>
2026-03-07 17:05:55 +0700

    update version

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
         description = "Ryusuke's Flake";
 
         inputs = {
-                nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+                nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
                 unstablepkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
                 home-manager = {
-                        url = "github:nix-community/home-manager/release-24.11";
+                        url = "github:nix-community/home-manager/release-25.11";
                         inputs.nixpkgs.follows = "nixpkgs";
                 };
         };
